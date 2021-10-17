d6f9dc70c5e426009015ae8117a72d170d279a71
jezerjojo14 <63692326+jezerjojo14@users.noreply.github.com>
2021-10-17 21:55:14 +0530

    added keyboard and drum visualizer

--- QuantumMusicComposer/composerapp/quantum_program.py
+++ QuantumMusicComposer/composerapp/quantum_program.py
@@ -7,10 +7,6 @@ from math import *
 import numpy as np
 
 
-def generate_composition(args):
-    data={}
-    return data
-
 def generate_melody(length, matrix=np.identity(8)):
     qc=QuantumCircuit(3)
     qc.h([0,1,2])
@@ -22,8 +18,7 @@ def generate_melody(length, matrix=np.identity(8)):
 
 
 
-def generate_beat(a=0.5, b=0.5, c=0.5, d=0.5, weight=4):
-
+def generate_rhythm(a=0.5, b=0.5, c=0.5, d=0.5, weight=4):
     qc=QuantumCircuit(4)
     qc.ry(a*pi,3)
     qc.ry(b*pi,2)
@@ -35,12 +30,50 @@ def generate_beat(a=0.5, b=0.5, c=0.5, d=0.5, weight=4):
     job_sim = backend_sim.run(transpile(qc, backend_sim), shots=weight)
     result_sim = job_sim.result()
     counts = result_sim.get_counts(qc)
-    return counts
+    ret_list=[int(key,2)/4.0 for key in counts.keys()]
+    ret_list.sort()
+    return ret_list
 
 
-kick_beats=generate_beat(a=0.5, b=0.3, c=0.2, d=0.1)
-print(kick_beats)
-snare_beats=generate_beat(a=0.5, b=0.8, c=0.2, d=0.1)
-print(snare_beats)
+def generate_composition(mood="happy"):
+    data={"C4": [8,9], "D4": [7,10,13,14], "E4": [0, 1, 6, 11, 12], "F4": [2,5], "G4": [3,4]}
+    max=0
+    for datum in data.keys():
+        for t in data[datum]:
+            if t>max:
+                max=t
+    kick_rhythm=generate_rhythm(a=0.5, b=0.3, c=0.2, d=0.1)
+    print(kick_rhythm)
+    for i in range(int(log2(max))-1):
+        kick_rhythm=kick_rhythm+[2**(i+2)+value for value in kick_rhythm]
+    data["kick"]=kick_rhythm
 
-generate_melody(4)
+    snare_rhythm=generate_rhythm(a=0.5, b=0.8, c=0.2, d=0.1)
+    print(snare_rhythm)
+    for i in range(int(log2(max))-1):
+        snare_rhythm=snare_rhythm+[2**(i+2)+value for value in snare_rhythm]
+    data["snare"]=snare_rhythm
+
+    hihat_rhythm=generate_rhythm(a=0.5, b=0.6, c=0.5, d=0.5, weight=15)
+    print(hihat_rhythm)
+    for i in range(int(log2(max))-1):
+        hihat_rhythm=hihat_rhythm+[2**(i+2)+value for value in hihat_rhythm]
+    data["hihat"]=hihat_rhythm
+
+    return data
+
+# print(generate_composition())
+
+
+# kick_rhythm=generate_rhythm(a=0.5, b=0.3, c=0.2, d=0.1)
+# print(kick_rhythm)
+# kick_rhythm=[int(key,2)/4.0 for key in kick_rhythm.keys()]
+# kick_rhythm.sort()
+# snare_rhythm=generate_rhythm(a=0.5, b=0.8, c=0.2, d=0.1)
+# print(snare_rhythm)
+# snare_rhythm=[int(key,2)/4.0 for key in snare_rhythm.keys()]
+# snare_rhythm.sort()
+# print(kick_rhythm)
+# print(snare_rhythm)
+#
+# generate_melody(4)
--- QuantumMusicComposer/composerapp/views.py
+++ QuantumMusicComposer/composerapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from . import quantum_program
 from django.http import HttpResponse, HttpResponseRedirect, Http404, JsonResponse
+import json
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 # Create your views here.
 
@@ -9,6 +11,11 @@ def index(request):
 
 def generate_composition(request):
     post_data = json.loads(request.body.decode("utf-8"))
-    args={}
-    data=quantum_program.generate_composition(args)
+    print(post_data)
+    data=quantum_program.generate_composition(post_data["mood"])
+    # data={"C4": [8,9], "D4": [7,10,13,14], "E4": [0, 1, 6, 11, 12], "F4": [2,5], "G4": [3,4]}
     return JsonResponse(data)
+
+@ensure_csrf_cookie
+def result(request, mood, tempo):
+    return render(request, "composerapp/result.html", {"preferences": json.dumps({"mood": mood, "tempo": tempo})})
