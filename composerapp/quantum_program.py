d6dd696c8b6972b6d8244beb7d64664b79905fdd
jezerjojo14 <63692326+jezerjojo14@users.noreply.github.com>
2021-10-18 22:29:26 +0530

    two transition matrices depending on major/minor scale. also added ability to compose music on any scale

--- composerapp/quantum_program.py
+++ composerapp/quantum_program.py
@@ -6,7 +6,23 @@ from qiskit.quantum_info.operators import Operator
 from math import *
 import numpy as np
 
-notes=["C4", "D4", "E4", "F4", "G4", "A5", "B5", "C5"]
+notes=["C4", "C#4", "D4", "Eb4", "E4", "F4", "F#4", "G4", "G#4", "A5", "Bb5", "B5", "C5"]
+major_steps=[0,2,2,1,2,2,2,1]
+minor_steps=[0,2,1,2,2,2,2,1]
+
+def st_number(note_number, scale_root, scale_type):
+    try:
+        pos=notes.index(scale_root+"4")
+    except:
+        pos=notes.index(scale_root+"5")
+
+    if scale_type=="M":
+        pos+=sum(major_steps[:note_number+1])
+    else:
+        pos+=sum(minor_steps[:note_number+1])
+
+    print(note_number, pos, pos-12*int(pos>=13))
+    return pos-12*int(pos>=13)
 
 def generate_rhythm(a=0.5, b=0.5, c=0.5, d=0.5, weight=4):
     qc=QuantumCircuit(4)
@@ -25,23 +41,24 @@ def generate_rhythm(a=0.5, b=0.5, c=0.5, d=0.5, weight=4):
     return ret_list
 
 
-def generate_melody(length, matrix):
+def generate_melody(length, matrix, scale_root="C", scale_type="M"):
 
-    rhythm=[]
-    i=0
+    rhythm=generate_rhythm(a=0.5, b=0.34, c=0.15, d=0.15, weight=20)
+    i=1
 
-    while len(rhythm)<length:
-        rhythm+=[t+4*i for t in generate_rhythm(a=0.4, b=0.3, c=0.2, d=0.2, weight=10)]
+    while rhythm[-1]<length-4:
+        rhythm+=[t+4*i for t in generate_rhythm(a=0.5, b=0.34, c=0.15, d=0.15, weight=20)]
         i+=1
 
-    rhythm=rhythm[:length]
 
-    length=int(length/2)
+    # rhythm=rhythm[:length]
+
+    # length=int(length/2)
 
     qc=QuantumCircuit(QuantumRegister(3), QuantumRegister(3))
     qc.h(list(range(6)))
     U=Operator(matrix)
-    for note in range(length):
+    for note in range(int(len(rhythm)/2)):
         qc.measure_all()
         qc.append(U, list(range(6)))
 
@@ -64,10 +81,12 @@ def generate_melody(length, matrix):
     melody_data={}
 
     i=0
+
     for t in measurement:
-        if notes[int(t,2)] not in melody_data.keys():
-            melody_data[notes[int(t,2)]]=[]
-        melody_data[notes[int(t,2)]]+=[rhythm[i]]
+        st_num=st_number(int(t,2),scale_root,scale_type)
+        if notes[st_num] not in melody_data.keys():
+            melody_data[notes[st_num]]=[]
+        melody_data[notes[st_num]]+=[rhythm[i]]
         i+=1
 
     return melody_data
@@ -83,20 +102,40 @@ def operator_matrix(transition_matrix):
     return W
 
 
+
 def generate_composition(mood="happy"):
 
-    # # Ode to Joy
-    # data={"C4": [8,9], "D4": [7,10,13,14], "E4": [0, 1, 6, 11, 12], "F4": [2,5], "G4": [3,4]}
-    transition_matrix=[
-    [1.2, 2.3, 1.2, 1.0, 1, 1, 0, 1],
-    [0.8, 0.2, 1.5, 1.0, 1, 1, 0, 1],
-    [1.2, 2.0, 0.5, 1.4, 1, 1, 0, 1],
-    [1.4, 1.5, 1.6, 1.0, 1, 1, 0, 1],
-    [1.8, 0.8, 1.0, 1.4, 1, 1, 2.5, 1],
-    [0.5, 0.6, 1.0, 1.0, 1, 1, 2.5, 1],
-    [0.1, 0.2, 0.2, 0.2, 1, 1, 0.5, 1],
-    [1.0, 0.4, 1.0, 1.0, 1, 1, 2.5, 1]]
-    data=generate_melody(60, operator_matrix((1/8.0)*np.transpose(np.array(transition_matrix))))
+    major_matrix=[
+    [0.09174311926605505, 0.1834862385321101, 0.1834862385321101, 0.09174311926605505, 0.1834862385321101, 0.11009174311926606, 0.06422018348623854, 0.09174311926605505],
+    [0.1769911504424779, 0.08849557522123895, 0.13274336283185842, 0.1504424778761062, 0.1769911504424779, 0.12389380530973451, 0.061946902654867256, 0.08849557522123895],
+    [0.1851851851851852, 0.1388888888888889, 0.0462962962962963, 0.1574074074074074, 0.1851851851851852, 0.12962962962962962, 0.06481481481481481, 0.0925925925925926],
+    [0.14184397163120566, 0.049645390070921974, 0.21276595744680848, 0.0070921985815602835, 0.21276595744680848, 0.14184397163120566, 0.021276595744680847, 0.21276595744680848],
+    [0.12318840579710144, 0.05072463768115942, 0.21739130434782608, 0.007246376811594203, 0.21739130434782608, 0.14492753623188406, 0.021739130434782608, 0.21739130434782608],
+    [0.1875, 0.125, 0.125, 0.125, 0.0625, 0.125, 0.0625, 0.1875],
+    [0.1, 0.13333333333333333, 0.06666666666666667, 0.0, 0.16666666666666666, 0.2, 0.0, 0.3333333333333333],
+    [0.14925373134328357, 0.07462686567164178, 0.07462686567164178, 0.07462686567164178, 0.29850746268656714, 0.07462686567164178, 0.14925373134328357, 0.1044776119402985]
+    ]
+
+    minor_matrix=[
+    [0.17467248908296945, 0.08733624454148473, 0.08733624454148473, 0.13100436681222707, 0.15283842794759828, 0.13100436681222707, 0.10480349344978167, 0.13100436681222707],
+    [0.08583690987124465, 0.10300429184549358, 0.1716738197424893, 0.15021459227467812, 0.12875536480686697, 0.08583690987124465, 0.1716738197424893, 0.10300429184549358],
+    [0.11764705882352941, 0.13725490196078433, 0.0784313725490196, 0.1568627450980392, 0.0784313725490196, 0.13725490196078433, 0.13725490196078433, 0.1568627450980392],
+    [0.15695067264573992, 0.17937219730941706, 0.10762331838565023, 0.08968609865470853, 0.1345291479820628, 0.1345291479820628, 0.08968609865470853, 0.10762331838565023],
+    [0.15444015444015444, 0.11583011583011583, 0.13513513513513514, 0.15444015444015444, 0.09266409266409266, 0.11583011583011583, 0.15444015444015444, 0.07722007722007722],
+    [0.09876543209876543, 0.1646090534979424, 0.12345679012345678, 0.09876543209876543, 0.1646090534979424, 0.14403292181069957, 0.0823045267489712, 0.12345679012345678],
+    [0.13953488372093023, 0.09302325581395349, 0.18604651162790697, 0.11627906976744186, 0.09302325581395349, 0.13953488372093023, 0.13953488372093023, 0.09302325581395349],
+    [0.16666666666666666, 0.16666666666666666, 0.125, 0.10416666666666667, 0.08333333333333333, 0.14583333333333334, 0.08333333333333333, 0.125]
+    ]
+
+    scale_type="M"
+
+    if mood in ["happy", "emotional", "hopeful"]:
+        transition_matrix=major_matrix
+    else:
+        transition_matrix=minor_matrix
+        scale_type="m"
+
+    data=generate_melody(16, operator_matrix(np.array(transition_matrix)), "C#", scale_type)
     # data=generate_melody(60, operator_matrix(np.eye(8)))
     print(data)
 
@@ -123,4 +162,7 @@ def generate_composition(mood="happy"):
         hihat_rhythm+=[4*i+value for value in hihat_rhythm_element]
     data["hihat"]=hihat_rhythm
 
-    return data
+
+
+    # {"solo_parts": {}, "chords": {"C": 0, "G": 4, "Am": 8, "F": 12, "G": 16, }}
+    return {"solo_parts": data, "chords": {}}
